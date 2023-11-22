252285382fcbd53c8221a32d26dae69a5958d614
ArlenP <arlen.pena@gmail.com>
2023-11-21 20:16:48 -0600

    Initial Commit

---
+++ CleanPokemon/CleanPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanPokemonApp.swift
+//  CleanPokemon
+//
+//  Created by Arlen Peña on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
